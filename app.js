f592f53df6dbe42819b838e2dd191436aeb60712
yemiwebby <yemiwebby@gmail.com>
2023-06-21 10:36:11 +0100

    updated app content

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Hello world" });
+  res.json({ message: "Hello world new people" });
 });
 
 module.exports = app;
